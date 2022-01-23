bed9db11b71916417e514bf9d8d5dd3d1be8cc79
KhurshedSaidov <asgfhsjsks@gmail.com>
2022-01-23 16:06:04 +0500

    add recipe

--- menu.php
+++ menu.php
@@ -1,2 +1,12 @@
-hello
+<?php
+
+/*
+    Recipe for cooking trout in the oven
+    2 trout
+    salt
+    pepper
+    lemon
+    2 garlic cloves
+    2 teaspoons vegetable oil
+    4 sprigs of dill
 
