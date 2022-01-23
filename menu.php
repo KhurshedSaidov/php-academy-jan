001b2f6defb171844dcf4957d4e1b0e87cb137a2
KhurshedSaidov <asgfhsjsks@gmail.com>
2022-01-23 16:07:53 +0500

    add function

--- menu.php
+++ menu.php
@@ -9,4 +9,16 @@
     2 garlic cloves
     2 teaspoons vegetable oil
     4 sprigs of dill
+*/
+
+function cookTrout(int $trout, float $salt, float $pepper, int $lemon, int $garlic, int $oilteaspoons, int $sprigsofdill)
+ {
+    echo " <br> рыбу промоем и выпотрошим, <br> посолим и поперчим снаружи и внутри, <br> лимон нарезать тонкими ломтиками, <br> чеснок нарезать небольшими кусочками, <br> польем форель маслом, <br> добавим лимон с чесноком и зелень,";
+    /*
+       Cover the baking dish with foil and put the fish on it
+       Wrap the carcass tightly in foil
+       Bake the fish for about 15 minutes at 200°C
+ }
+
+
 
